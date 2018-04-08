2a94f16ccfb80603ee7e6908fa18797829af9173
Vanessa Henson <vanessahenson@Vanessas-Air.hsd1.co.comcast.net>
2018-04-08 17:57:58 -0600

    Added the first part of the weather app

---
+++ weather-app/playground/callback.js
@@ -0,0 +1,15 @@
+let getUser = (id, callback) => {
+	let user = {
+		id: id,
+		name: 'Vanessa'
+	};
+
+	setTimeout(()=>{
+		callback(user);
+	}, 3000);
+
+};
+
+getUser(31, (user) =>{
+	console.log(user);
+});
