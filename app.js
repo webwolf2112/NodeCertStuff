f0f6d79d8aaedbc64e5bb796f0affd7b89503cf0
Vanessa Henson <vhenson@proofpoint.com>
2020-07-06 16:22:52 -0600

    Added some examples for practice

---
+++ app.js
@@ -0,0 +1,15 @@
+const hello = () => {
+    let sayHi = 'hello world'
+
+    sayHi = 'yo';
+
+    sayHi = 'hello';
+    sayHi = 'wold';
+    sayHi = 'yasdf';
+    sayHi = 'hgf';
+    sayHi = 'sdfg';
+
+    console.log( process.argv[2] );
+};
+
+hello();
